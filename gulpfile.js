e073decd7ef10588033d99e5ddd56a048b3e3b01
BeEasy <monkofpain@gmail.com>
2017-02-10 18:01:10 +0200

    lastupd

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
 // Requires the gulp-sass plugin
 var sass = require('gulp-sass');
+var clean        = require('gulp-clean');
 var autoprefixer = require("gulp-autoprefixer");
 var browserSync = require('browser-sync').create();
 var sourcemaps   = require('gulp-sourcemaps');
 var reload      = browserSync.reload;
 var spritesmith = require('gulp.spritesmith');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglifyjs');
+var cssnano = require('gulp-cssnano');
+var rename = require('gulp-rename');
 
 
 gulp.task('browserSync', function() {
@@ -21,7 +26,7 @@ gulp.task('sass', function(){
     .pipe(sourcemaps.init())
     .pipe(sass({outoutStyle: 'expanded'}).on('error', sass.logError)) // Using gulp-sass
     .pipe(autoprefixer({ //autoprefixer.
-      browsers: ['last 15 versions']
+      browsers: ['last 15 versions', '> 1%', 'ie 8', 'ie 7']
     }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('app/css'))
@@ -30,9 +35,25 @@ gulp.task('sass', function(){
     }))
 });
 
+gulp.task('scripts', function () {
+  return gulp.src([
+      'app/libs/jquery/dist/jquery.min.js',
+    ])
+  .pipe(concat('libs.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('app/js'))
+})
+
+gulp.task('css-libs', ['sass'], function(){
+  return gulp.src('app/css/main.css')
+  .pipe(cssnano())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('app/css'))
+})
+
 gulp.task('sprite', function() {
     var spriteData = 
-        gulp.src('src/sprites/*.*') // путь, откуда берем картинки для спрайта
+        gulp.src('app/src/sprites/*.*') // путь, откуда берем картинки для спрайта
             .pipe(spritesmith({
                 imgName: 'sprite.png',
                 cssName: 'sprite.sass',
@@ -45,10 +66,29 @@ gulp.task('sprite', function() {
     spriteData.css.pipe(gulp.dest('app/scss/')); // путь, куда сохраняем стили
 });
 
-gulp.task('watch',['sass', 'browserSync'], function(){
-  gulp.watch('src/sprites/*.*', ['sprite']);
+gulp.task('watch',['css-libs', 'browserSync', 'scripts'], function(){
+  gulp.watch('app/src/sprites/*.*', ['sprite']);
   gulp.watch(['app/scss/*.scss', 'app/scss/*.sass'], ['sass']); 
   gulp.watch("app/*.html").on("change", reload);
+  gulp.watch("app/js/**/*.js").on("change", reload);
 })
 
 gulp.task('default', ['sprite' ,'watch', 'sass']);
+
+
+gulp.task('clean', function() {
+  return gulp.src('dist/')
+    .pipe(clean());
+});
+
+
+
+gulp.task('build', ['clean', 'sass', 'scripts'], function() {
+
+  var buildJs = gulp.src('app/js/**/*').pipe(gulp.dest('dist/js'));
+  var buildCss = gulp.src('app/css/**/*').pipe(gulp.dest('dist/css'));
+  var buildFonts = gulp.src('app/fonts/**/*').pipe(gulp.dest('dist/fonts'));
+  var buildImg = gulp.src('app/img/**/*').pipe(gulp.dest('dist/img'));
+  var buildHtml = gulp.src('app/*.html').pipe(gulp.dest('dist/'));
+
+});
